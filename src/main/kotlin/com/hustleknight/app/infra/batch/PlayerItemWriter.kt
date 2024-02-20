6a9e350769ce11e724f6395422dabf959e26066d
Woozy <anfrosus@naver.com>
2024-02-20 14:36:18 +0900

    fix: project directory name

--- src/main/kotlin/com/hustleknight/app/infra/batch/PlayerItemWriter.kt
+++ src/main/kotlin/com/hustleknight/app/infra/batch/PlayerItemWriter.kt
@@ -1,21 +1,21 @@
-//package com.hustleknight.app.infra.batch
-//
-//import com.hustleknight.app.model.Player
-//import com.hustleknight.app.repository.PlayerRepository
-//import org.springframework.batch.item.Chunk
-//import org.springframework.batch.item.ItemWriter
-//import org.springframework.stereotype.Component
-//
-//@Component
-//class PlayerItemWriter(
-//    private val playerRepository: PlayerRepository
-//): ItemWriter<Player> {
-//    val count = 1L
-//
-//    override fun write(chunk: Chunk<out Player>) {
-////        chunk.map {
-////            it.emdtn = count
-////        }
-//        playerRepository.saveAll(chunk.items)
-//    }
-//}
+package com.hustleknight.app.infra.batch
+
+import com.hustleknight.app.model.Player
+import com.hustleknight.app.repository.PlayerRepository
+import org.springframework.batch.item.Chunk
+import org.springframework.batch.item.ItemWriter
+import org.springframework.stereotype.Component
+
+@Component
+class PlayerItemWriter(
+    private val playerRepository: PlayerRepository
+): ItemWriter<Player> {
+    val count = 1L
+
+    override fun write(chunk: Chunk<out Player>) {
+//        chunk.map {
+//            it.emdtn = count
+//        }
+        playerRepository.saveAll(chunk.items)
+    }
+}
